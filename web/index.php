5b412dac2e224aed82aeb44f5121e823653da6ec
Taylor Bailey <taylor-bailey@live.com>
2019-05-04 10:33:56 -0600

    partial

--- web/index.php
+++ web/index.php
@@ -3,3 +3,36 @@
   phpinfo();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <h1>Taylor's Homepage</h1>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <h3>welcome to my homepage!</h3>
+    <img src="./images/photo.JPG" alt="Taylor Bailey">
+    <p>
+      I study at Brigham Young University - Idaho, I am a software engineer with a focus on full stack development.
+      In my educational career I have learned to write professional code, present myself in both a technical and professional setting,
+      and work together with classmates to complete projects within deadlines. I have been fascinated with computers their 
+      inner-workings since I was young. Often I found myself switching components in the computer to make it faster, 
+      sometimes discovering that some of the components didn't make a difference. I later became interested in the software side of my computer;
+      eventually, looking up h  ow to learn to code on my own. It wasn't until college that I found a true passion for software development,
+      more specifically web development.
+    </p>
+  </main>
+</body>
+</html>
